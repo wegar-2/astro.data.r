1d55871f00625252e0a6192e963277f390b91650
Artur Wegrzyn <awegrzyn17@gmail.com>
2021-03-06 15:02:56 +0100

    Renamed project and repo

--- R/OceanAtmosphereOscillationsFunctions.R
+++
@@ -1,9 +0,0 @@
-# ------------------------------------------------------------------------------
-# Functions for fetching data on Ocean-Atmospheric Oscillation indices from
-# NOAA website: https://psl.noaa.gov/data/timeseries/daily/list/
-# ------------------------------------------------------------------------------
-
-lDownloadOceanAtmosphericOscillationIndices <- function() {
-
-}
-
